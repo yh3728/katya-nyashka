7ffe95bd1523888d9fa0017b4bd450e3847c446f
yokomin12 <131192821+yh3728@users.noreply.github.com>
2026-05-11 22:24:30 +0300

    а ой

--- js/scroll.js
+++ js/scroll.js
@@ -1,37 +1,38 @@
 const gallery = document.querySelector('#gallery');
 
 window.addEventListener('scroll', () => {
-    handleScroll(gallery, gallery, null, [-2000, 0], 1, 0.5);
+    handleScroll(gallery, gallery, null, [-2500, 0]);
 })
 
 // moveContainer - контейнер, что мы двигаем,
 // observedElement - элемент, который мы при скролле отслеживаем,
 // rotate - угол, на который вертим, translate - сдвиг вправо или вниз в пикселях
-// beginCoefficient - с какой части контейнера начинается движение
-function handleScroll(moveContainer, observedElement, rotate, translate, beginCoefficient, endCoefficient) {
-    const rect = observedElement.getBoundingClientRect();
+function handleScroll(moveContainer, observedElement, rotate, translate) {
+    const obs = observedElement.getBoundingClientRect();
+    const move = moveContainer.getBoundingClientRect();
     const windowHeight = window.innerHeight;
 
-    // // 0 - элемент вне экрана, 1 - полностью виден
-    let progress = 1 - ((rect.top + (rect.height * beginCoefficient)) / windowHeight);
-    let progress2 = ((rect.bottom - rect.height * endCoefficient) / windowHeight);
-    console.log(progress, progress2);
+    const start = obs.bottom - windowHeight;
+    const end = obs.top;
+
+    const distance = end - start || 1;
+
+    let progress = (0 - start) / distance;
 
     if (progress < 0) progress = 0;
     if (progress > 1) progress = 1;
 
-    // смещаем или крутим в зависимости от скролла
-    // создаём массив трансформаций
-    let transforms = [];
+    const transforms = [];
 
     if (rotate) {
         transforms.push(`rotate(${progress * rotate}deg)`);
     }
 
     if (translate) {
-        transforms.push(`translate(${progress * translate[0]}px, ${progress * translate[1]}px)`);
+        transforms.push(
+            `translate(${progress * translate[0]}px, ${progress * translate[1]}px)`
+        );
     }
 
-    // объединяем в одну строку
     moveContainer.style.transform = transforms.join(" ");
 }
